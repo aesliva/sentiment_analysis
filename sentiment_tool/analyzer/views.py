ba7c0457631723d43e8214ed4dba4ad4091c80c1
Aesli <aesliva0@gmail.com>
2024-10-24 21:07:27 -0500

    Create simple view for feedback input

--- sentiment_tool/analyzer/views.py
+++ sentiment_tool/analyzer/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import JsonResponse
+from .sentiment_analysis import predict_sentiment, train
+
+# train the model separately and save it
+train()
+
+def analyze(request):
+    if request.method == "GET":
+        user_text = request.GET.get("feedback")
+        if user_text:
+            sentiment = predict_sentiment(user_text)
+            return JsonResponse({"sentiment": "positive" if sentiment[0] == 1 else "negative"})
+        return JsonResponse({"error": "No feedback provided"}, status=400)
