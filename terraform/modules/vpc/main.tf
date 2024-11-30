4ed4ac1bd62eab635c33c0fe0105584852d3c42f
Aesli <aesliva0@gmail.com>
2024-11-30 17:51:33 -0600

    Associate VPC route tables with subnets

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -48,4 +48,10 @@ resource "aws_route_table" "public" {
     Name        = "${var.project_name}-public-rt-${var.environment}"
     Environment = var.environment
   }
+}
+
+resource "aws_route_table_association" "public" {
+  count          = 2
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
 } 
