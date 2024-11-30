d2107aee95020bd8a18ef811073c5b03e81a5152
Aesli <aesliva0@gmail.com>
2024-11-30 17:17:01 -0600

    Add EC2 variables config

---
+++ terraform/modules/ec2/variables.tf
@@ -0,0 +1,34 @@
+variable "project_name" {
+  description = "Name of the project"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment (dev/staging/prod)"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "VPC ID"
+  type        = string
+}
+
+variable "subnet_id" {
+  description = "Subnet ID"
+  type        = string
+}
+
+variable "key_name" {
+  description = "Name of the SSH key pair"
+  type        = string
+}
+
+variable "security_group_id" {
+  description = "Security group ID"
+  type        = string
+}
